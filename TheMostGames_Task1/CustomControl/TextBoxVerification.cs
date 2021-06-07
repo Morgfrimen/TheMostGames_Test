1eb86846f459ca6e131464f46cf2e5ef69e4d07b
Дрожкин Александр Андреевич <Aleksandr.Drozhkin@ferrobuilding.ru>
2021-06-07 17:34:07 +0300

    Add Custom TextBox

---
+++ TheMostGames_Task1/CustomControl/TextBoxVerification.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TheMostGames_Task1.CustomControl
+{
+
+	internal sealed class TextBoxVerification : TextBox
+	{
+		private static DependencyProperty _minIndexProperty;
+		private static DependencyProperty _maxIndexProperty;
+		private static DependencyProperty _textProperty;
+
+		static TextBoxVerification()
+		{
+			_minIndexProperty = DependencyProperty.Register(nameof(MixIndex),typeof(int),typeof(TextBoxVerification));
+			_maxIndexProperty = DependencyProperty.Register(nameof(MaxIndex), typeof(int), typeof(TextBoxVerification));
+			_textProperty = DependencyProperty.Register(nameof(Text),typeof(string),typeof(TextBoxVerification));
+		}
+
+		internal int MaxIndex
+		{
+			get => (int)GetValue(_maxIndexProperty);
+			set => SetValue(_maxIndexProperty,value);
+		}
+
+		internal int MixIndex
+		{
+			get => (int)GetValue(_minIndexProperty);
+			set => SetValue(_minIndexProperty, value);
+		}
+
+		internal new string Text
+		{
+			get => (string)GetValue(_textProperty);
+			set => SetValue(_textProperty, value);
+		}
+	}
+
+}
